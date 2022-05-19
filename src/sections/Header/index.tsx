95b275fbbfa449c99af50224f4a663df8d6c91cf
Brian Kim <gittybrian@gmail.com>
2022-05-18 23:52:46 -0400

    added: header section

---
+++ src/sections/Header/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './styles.module.scss';
+
+const Header: React.FunctionComponent = () => {
+    return <div className={styles.container}>
+        <h1>brian hee kim</h1>
+    </div>
+}
+
+export default Header;
