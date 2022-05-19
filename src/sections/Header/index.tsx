448c866e77390e32c8bfec43c739966abb6bd21e
Brian Kim <gittybrian@gmail.com>
2022-05-19 00:38:09 -0400

    styling: header section

--- src/sections/Header/index.tsx
+++ src/sections/Header/index.tsx
@@ -4,6 +4,7 @@ import styles from './styles.module.scss';
 const Header: React.FunctionComponent = () => {
     return <div className={styles.container}>
         <h1>brian hee kim</h1>
+        <h2>self-taught web developer</h2>
     </div>
 }
 
