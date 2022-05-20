31d513538d032110b69f69c8434d514d51d5da2f
Brian Kim <gittybrian@gmail.com>
2022-05-20 12:16:00 -0400

    styling: stack animation

--- src/sections/Stack/index.tsx
+++ src/sections/Stack/index.tsx
@@ -1,11 +1,31 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import STACK from './constants';
 import styles from './styles.module.scss';
 
 const Stack: React.FunctionComponent = () => {
+    const stackRef = useRef<HTMLDivElement>(null)
+
+    const observer = new IntersectionObserver(
+		(entries) => {
+			entries.forEach((entry) => {
+				if (entry.isIntersecting) {
+					entry.target.classList.add(styles.animate);
+					observer.unobserve(entry.target);
+					return;
+				}
+			});
+		},
+		{ rootMargin: '-20% 0% -20% 0%' }
+	);
+
+
+	useEffect(() => {
+		observer.observe(stackRef.current!)
+    }, []);
+
     return <div className={styles.container}>
         <h2>my current tech stack</h2>
-        <div className={styles.stack}>
+        <div className={styles.stack} ref={stackRef}>
         {STACK.map((tech, idx) => (
             <a href={tech.href} key={idx}>
                 <img src={tech.logo} alt={tech.alt} />
