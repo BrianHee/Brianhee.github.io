d1dca69daf91ff4f0b19837dd61980d192f843c7
Brian Kim <gittybrian@gmail.com>
2022-07-11 23:16:47 -0400

    added: work section

---
+++ src/sections/Work/constants.ts
@@ -0,0 +1,17 @@
+interface Work {
+    title: string;
+    company: string;
+}
+
+const WORK: Work[] = [
+    {
+        title: 'software developer',
+        company: 'ClassLink'
+    },
+    {
+        title: 'research associate',
+        company: 'Yale Medical School'
+    }
+]
+
+export default WORK;
