d08089745e05c235cea748adc3e8223bee882dc2
Brian Kim <gittybrian@gmail.com>
2022-07-12 21:55:38 -0400

    added: years to work interface

--- src/sections/Work/constants.ts
+++ src/sections/Work/constants.ts
@@ -1,16 +1,19 @@
 interface Work {
     title: string;
     company: string;
+    years: string
 }
 
 const WORK: Work[] = [
     {
         title: 'software developer',
-        company: 'ClassLink'
+        company: 'ClassLink',
+        years: '2022 - present'
     },
     {
         title: 'research associate',
-        company: 'Yale Medical School'
+        company: 'Yale Medical School',
+        years: '2019 - 2021'
     }
 ]
 
