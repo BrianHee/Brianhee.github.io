f1996ed7b37167f1e6b6d27f239f7410e9bc4d61
Brian Kim <gittybrian@gmail.com>
2024-04-23 10:18:04 -0400

    update: fonts and portrait

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,6 @@ function App() {
     <div className="App">
       <Header />
       <About />
-      <Stack />
       <Projects />
       <Footer />
     </div>
--- src/sections/Header/index.tsx
+++ src/sections/Header/index.tsx
@@ -1,20 +1,28 @@
 import React from 'react';
 import styles from './styles.module.scss';
-
 import SOCIALS from './constants';
+import portrait from '../../assets/portrait.svg';
 
 const Header: React.FunctionComponent = () => {
-    return <div className={styles.container}>
-        <h1>brian kim</h1>
-        <h2>software developer</h2>
-        <div className={styles.links}>
-            {SOCIALS.map((social, idx) => (
-                <a href={social.href} key={idx}>
-                    <img src={social.logo} alt={social.alt}/>
-                </a>
-            ))}
+    return (
+        <div className={styles.container}>
+            <div>
+                <h1>brian kim</h1>
+                <h2>software developer</h2>
+                <div className={styles.links}>
+                    {SOCIALS.map((social, idx) => (
+                        <a href={social.href} key={idx}>
+                            <img src={social.logo} alt={social.alt}/>
+                        </a>
+                    ))}
+                </div>
+            </div>
+            <div className={styles['portrait-container']}>
+                <div className={styles['portrait-background']}>
+                </div>
+            </div>
         </div>
-    </div>
+    )
 }
 
 export default Header;
